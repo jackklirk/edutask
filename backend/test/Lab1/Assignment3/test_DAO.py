263104f5d3dc33b9a4ddb771f3e90bd62de444be
jackklirk <angelopradojr@gmail.com>
2024-04-21 20:24:02 +0200

    Clean up commentaries

--- backend/test/Lab1/Assignment3/test_DAO.py
+++ backend/test/Lab1/Assignment3/test_DAO.py
@@ -20,10 +20,6 @@ class TestCreation:
     # System under test will be a DAO object from the file dao.py
     @pytest.fixture
     def sut(self):
-        # Julian said that it is good to create entities on db when testing, to not rely on existing ones.
-        # We can mock a new entity and then delete it from the DB
-        # I am basing on the slide 16
-        # Update: I tried to create a file, but for some reason windows does not allow me to do so through pytest
         fabricated_collection = 'mocked_collection'
         fabricated_validator = {
                                 "$jsonSchema": {
@@ -57,7 +53,7 @@ class TestCreation:
             database = client.edutask
             # Drop collection if it exist to make it sure we are starting with a clean one
             database[fabricated_collection].drop()
-            #database[fabricated_collection].create_index([("PN", 1)], unique=True)
+            
             # Create collection (since it does not exists) and return
             yield DAO(fabricated_collection)
             
@@ -104,9 +100,6 @@ class TestCreation:
         with pytest.raises(pymongo.errors.WriteError) as excinfo:
             sut.create(test_data)
 
-
-    # This one fails problably because unique keys should have in app validation, not just on json
-    # https://www.mongodb.com/community/forums/t/how-to-make-schema-field-unique/107585 
     @pytest.mark.assignment3
     def test_id4_creation_fail_2(self, sut):
         """
@@ -115,7 +108,7 @@ class TestCreation:
         test_data = {
             "description": "Some more mocked data",
             "mock": True,
-            "PN": 2000000000
+            "PN": 2000000002
         }
 
         # First creation should work
@@ -128,11 +121,11 @@ class TestCreation:
     @pytest.mark.assignment3
     def test_id5_creation_fail_3(self, sut):
         """
-        Creating with wrong datatypes values on keys
+        Create with wrong datatypes values on keys
         """
         test_data = {
             "description": 0.5,
-            "mock": 1,
+            "mock": 9,
             "PN": True
         }
 
@@ -147,11 +140,8 @@ class TestCreation:
         test_data = {
         }
 
-        # First creation should work
-        # Second should raise error since description is unique
         with pytest.raises(pymongo.errors.WriteError) as excinfo:
             sut.create(test_data)
-
 
     @pytest.mark.assignment3
     def test_id7_creation_fail_4(self, sut):
@@ -159,66 +149,5 @@ class TestCreation:
         No argument provided
         """
 
-        # Second should raise error since description is unique
         with pytest.raises(TypeError) as excinfo:
                 sut.create()
-
-
-
-
-
-# import pymongo.mongo_client
-# import pytest
-# import pymongo 
-# import os
-# import json
-# from dotenv import dotenv_values
-# from unittest.mock import patch, MagicMock
-# from src.util.dao import DAO
-# from src.util.validators import getValidator
-
-
-
-# @pytest.fixture
-# def database():
-#     Local_Mongo_URL = dotenv_values('.env').get('MONGO_URL')
-#     Mongo_URL = os.environ.get('MONGO_URL', Local_Mongo_URL)
-
-#     client = pymongo.MongoClient(Mongo_URL)
-#     return client
-
-
-# @pytest.mark.assignment3
-# def test_mongodb_connection(database): 
-    
-#     assert database.admin.command("ping")["ok"] != 0.0
-
-# @pytest.fixture
-# def database_rollback(database):
-#     session = database.start_session()
-#     session.start_transaction()
-#     try: 
-#         yield session
-
-#     finally:
-#         session.abort_transaction()
-
-
-
-# @pytest.mark.assignment3
-# @pytest.mark.parametrize('collection_name, data',[('test', {'description':'Testing functionality'})])
-# def test_create_success(database, database_rollback, collection_name, data): 
-#     i = 1
-#     d = 2
-#     db = database.edutask
-#     with open('backend/test/Lab1/Assignment3/static/test.json') as f:
-#         data = json.load(f)
-
-#     print(db.list_collection_names())
-#     with patch('src.util.dao.getValidator') as mockedValidator:
-#         mockedValidator.return_value = data
-#         dao = DAO(collection_name)
-        
-#         assert dao.create()
-
-
