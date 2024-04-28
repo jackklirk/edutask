4d6f7999d226cae04e2332920f82931c3d4ab15c
cabbe <perle@CABBESDEMON>
2024-04-28 17:07:00 +0200

    Co-authored-by: Angelo Prado <jackklirk@users.noreply.github.com>

--- frontend/cypress/e2e/lab2/assignment4.cy.js
+++ frontend/cypress/e2e/lab2/assignment4.cy.js
@@ -1,10 +1,96 @@
 describe('R8UC1', () => {
-
   let uid
   let name
   let email
-  let tid
   
+  before(function() {
+    cy.fixture('test_user.json').then((user) => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:5000/users/create',
+        form: true,
+        body: user
+      }).then((response) => {
+        uid = response.body._id.$oid
+        name = user.firstName + ' ' + user.lastName
+        email = user.email
+        // then() works async, it runs just after the previous statment 
+        // looks like the request must be in the "then()" to the id to be defined
+        // https://docs.cypress.io/guides/core-concepts/variables-and-aliases#Closures
+        cy.request({
+          method: 'POST',
+          url: `http://localhost:5000/tasks/create`,
+          form: true,
+          body: {
+            title: "talk to IT",
+            description: "hello",
+            url: "U_gANjtv28g",
+            todos: 'Watch',
+            userid: uid
+          }
+        })
+      })
+    })
+  })
+  
+  beforeEach(function () {
+      // enter the main main page
+      cy.visit('http://localhost:3000')
+      cy.contains('div', 'Email Address')
+      .find('input[type=text]')
+      .type(email)
+      cy.get('form')
+      .submit()
+      cy.get('h1')
+      .should('contain.text', 'Your tasks, ' + name)
+  })
+  it('checking if the task exists', () =>{
+    cy.get('div.title-overlay')
+      .should('contain.text', 'talk to IT')
+  })
+      
+  it('ID 1 - opening the task', () => {
+    cy.contains('div.title-overlay', 'talk to IT')
+    .click()
+
+    cy.get('p span.editable')
+    .should('contain.text', 'hello')
+
+    cy.get('h1 span.editable')
+    .should('contain.text', 'talk')
+  })
+
+  it('ID 2 - Add empty todo item', () =>{
+    cy.contains('div.title-overlay', 'talk to IT')
+    .click()
+    cy.get('form.inline-form').find('input[type=submit]').click()
+    cy.get('li.todo-item').should('have.length', 1)
+  })
+
+  it('ID 3 - Add a todo item with description', () =>{
+    cy.contains('div.title-overlay', 'talk to IT')
+    .click()
+    cy.get('form.inline-form').find('input[type=text]').type('Ask why fans speed up')
+    cy.get('form.inline-form').find('input[type=submit]').click()
+    cy.contains('li.todo-item', 'Ask why fans speed up')
+  })
+
+    after(function () {
+      cy.request({
+        method: 'DELETE',
+        url: `http://localhost:5000/users/${uid}`
+      }).then((response) => {
+        cy.log(response.body)
+      })    
+    })
+  
+  })
+
+describe('R8UC2', () => { 
+  let uid
+  let name
+  let email
+
   before(function() {
     cy.fixture('test_user.json').then((user) => {
       cy.request({
@@ -24,75 +110,18 @@ describe('R8UC1', () => {
           url: `http://localhost:5000/tasks/create`,
           form: true,
           body: {
-            title: "talk to IT",
+            title: "talk to IT",  
             description: "hello",
             url: "U_gANjtv28g",
-            // This was also the problem, if we send a list(like it is in the dummy) it crashes *facepalm*
-            // todos: [
-            //   "Watch video", 
-            //   "Evaluate usability of tools", 
-            //   "Check out BundlePhobia and investigate npm packages in most recent projects"
-            // ],
-            todos: "watch video",
+            todos: "Watch",
             userid: uid
           }
-        }).then((response) => {
-          //tid = response[0].body._id.$oid
-          // After thinking for a while I am not sure if we really need the task id
-          tid = response.body[0]._id.$oid
         })
       })
     })
   })
-  // RESPONSE OF THE TASK CREATION ON POSTMAN
-  //   [
-  //     {
-  //         "_id": {
-  //             "$oid": "66298c883016dc0f54698142"
-  //         },
-  //         "categories": [],
-  //         "description": "\"How to tie a tie\"",
-  //         "startdate": {
-  //             "$date": "2024-04-25T00:49:44.366Z"
-  //         },
-  //         "title": "\"Tie a tie\"",
-  //         "todos": [
-  //             {
-  //                 "_id": {
-  //                     "$oid": "66298c883016dc0f54698141"
-  //                 },
-  //                 "description": "\"watch video\"",
-  //                 "done": false
-  //             }
-  //         ],
-  //         "video": {
-  //             "_id": {
-  //                 "$oid": "66298c883016dc0f54698140"
-  //             },
-  //             "url": "\"U_dasjndjank\""
-  //         }
-  //     }
-  // ]
-    
-    // cy.fixture('test_task.json').then((task) => {
-    //   cy.request({
-    //     method: 'POST',
-    //     url: `http://localhost:5000/tasks/create`,
-    //     form: true,
-    //     body: task
-    //   }).then((response) => {
-    //     tid = response.body._id.$oid
-    //   })
-    // })
 
-      // let task = {
-      //   "title": "talk to IT",
-      //   "description": "hello",
-      //   "url": "U_gANjtv28g",
-      //   "userid": uid
-      // }
-  
-    beforeEach(function () {
+  beforeEach(function () {
       // enter the main main page
       cy.visit('http://localhost:3000')
       cy.contains('div', 'Email Address')
@@ -102,30 +131,11 @@ describe('R8UC1', () => {
       .submit()
       cy.get('h1')
       .should('contain.text', 'Your tasks, ' + name)
-    })
-    
-    
-    it('checking if the task exists', () =>{
-      // a hint here is to have the application on the browser and inspect the desired
-      // elements with the right click
-      cy.get('div.title-overlay')
-        .should('contain.text', 'talk to IT')
+      cy.contains('div.title-overlay', 'talk to IT')
+      .click()
     })
       
-    it('opening the task', () => {
-      cy.get('img[src="http://i3.ytimg.com/vi/U_gANjtv28g/hqdefault.jpg"]')
-      .click()
-
-      cy.get('p span.editable')
-      .should('contain.text', 'hello')
-
-      cy.get('h1 span.editable')
-      .should('contain.text', 'talk')
-
-      cy.get('li.todo-item')
-      .should('contain.text', 'watch')
-    })
-
+    
     // beforeEach(function () {
     //   // enter the main main page
     //   cy.visit('http://localhost:3000')
@@ -138,26 +148,92 @@ describe('R8UC1', () => {
     //   //cy.get('img')
     //   .click()
     // })
-       
-    it('toggle existing todo', () => {
-      cy.get('img[src="http://i3.ytimg.com/vi/U_gANjtv28g/hqdefault.jpg"]')
-      .click()
 
-      cy.get('span.checker.unchecked')
-      .click()
-
-      cy.get('span.checker.checked').should('exist')
-
-    })
-
+  it('ID 1 - Mark todo as done', () => {
     
+
+    cy.get('span.checker.unchecked')
+    .click()
+
+    cy.get('span.checker.checked').should('exist')
+
+  })
+
+  it('ID 2 - Unmark todo', () => {
+    
+
+    cy.get('span.checker.checked')
+    .click()
+
+    cy.get('span.checker.unchecked').should('exist')
+
+  })
+
   after(function () {
     // clean up by deleting the user from the database
     cy.request({
       method: 'DELETE',
       url: `http://localhost:5000/users/${uid}`
-    }).then((response) => {
-      cy.log(response.body)
+    })
+  })
+})
+
+describe('R8UC3', () => { 
+  let uid
+  let name
+  let email
+
+  before(function() {
+    cy.fixture('test_user.json').then((user) => {
+      cy.request({
+        method: 'POST',
+        url: 'http://localhost:5000/users/create',
+        form: true,
+        body: user
+      }).then((response) => {
+        uid = response.body._id.$oid
+        name = user.firstName + ' ' + user.lastName
+        email = user.email
+        cy.request({
+          method: 'POST',
+          url: `http://localhost:5000/tasks/create`,
+          form: true,
+          body: {
+            title: "talk to IT",  
+            description: "hello",
+            url: "U_gANjtv28g",
+            todos: "Watch",
+            userid: uid
+          }
+        })
+      })
+    })
+  })
+
+  beforeEach(function () {
+      // enter the main main page
+      cy.visit('http://localhost:3000')
+      cy.contains('div', 'Email Address')
+      .find('input[type=text]')
+      .type(email)
+      cy.get('form')
+      .submit()
+      cy.get('h1')
+      .should('contain.text', 'Your tasks, ' + name)
+      cy.contains('div.title-overlay', 'talk to IT')
+      .click()
+    })
+
+  it('ID 1 - deleting existing todo', () => {
+    cy.get('span.remover')
+    .click().then(cy.get('li.todo-item').should('not.exist'))
+  })
+
+  after(function () {
+    // clean up by deleting the user from the database 
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/users/${uid}`
     })
   })
 })
